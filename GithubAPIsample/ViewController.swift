043f288236d42b9eb5013cdc757a6bd184a2b5cd
yamatohagi <la.luce.ymt0326@gmail.com>
2019-08-22 13:51:45 +0900

    Initial Commit

---
+++ GithubAPIsample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubAPIsample
+//
+//  Created by 萩　山登 on 2019/08/22.
+//  Copyright © 2019 萩　山登. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
